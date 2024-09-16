a9e8bf191a931ecb824cdad587d660e9ce05ec84
linda_a <larmstrongnz@gmail.com>
2024-09-16 23:16:09 +1200

    Added changes to button when clicked

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ var button = document.querySelector(".show-cat");
 // console.log(button);
 
 var cat = document.querySelector(".cat");
-console.log(cat);
+// console.log(cat);
 
 button.addEventListener("click", function() {
     // console.log("It's working!");
-    cat.classList.add("show");
-}
+    if (cat.classList.contains("show")) {
+        // console.log("Yes!"); 
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    }
+    else {
+        cat.classList.add("show");
+        button.innerText = "Shoo cat!!";
+        button.classList.remove("disappear");
+        // (console.log("No!"));
+    } 
+});
 
-)
